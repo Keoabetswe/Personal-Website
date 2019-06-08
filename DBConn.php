aad057ec121adae33d3b90d3614a0926aaa3c447
Keo <KeoNthite@gmail.com>
2019-06-08 10:19:45 +0200

    added DB connection strings

--- DBConn.php
+++ DBConn.php
@@ -1,7 +1,9 @@
 <?php 	
-$db_host = "localhost";	
-$db_username = "root";	
-$db_password = "";	
+
+$db_host="localhost";	
+$db_username="keontuzb_keo";	
+$db_password="Mm@mpeta1";	
+$db_name="keontuzb_website";
 
 $db_connect = mysqli_connect($db_host, $db_username, $db_password);	
 
