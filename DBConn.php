3105c525ba67beb01f2319742f6aae3a09a053ac
Keo <KeoNthite@gmail.com>
2019-06-27 21:54:23 +0200

    changed database connection string

--- DBConn.php
+++ DBConn.php
@@ -1,9 +1,9 @@
 <?php 	
 
 $db_host="localhost";	
-$db_username="root";	
-$db_password="";	
-$db_name="personal_website";
+$db_username="keontuzb";	
+$db_password="keontuzb_keo";	
+$db_name="keontuzb_website";
 
 $db_connect = mysqli_connect($db_host, $db_username, $db_password);	
 
