a2e0ef46d0a2719109095a4b54140f153440aada
Keo <KeoNthite@gmail.com>
2019-07-13 15:48:34 +0200

    Changed slider headings

--- js/slider.js
+++ js/slider.js
@@ -4,43 +4,57 @@ function _(x){return document.getElementById(x);}
 var ba, bi=0, intrvl;
 // bca - Bubble Content Array. Put your content here.
 var bca = [
-    '<h2>Heading Number 1</h2><p>Skills</p>',
-	'<h2>Heading Number 2</h2><p>Qualification</p>',
-	'<h2>Heading Number 3</h2><p>Content for section 3</p>',
+    '<h2>Skills</h2><p>placeholder 1</p>',
+	'<h2>Qualification</h2><p>placeholder 2</p>',
+	'<h2>Experience</h2><p>placeholder 3</p>',
 	
 ];
+
 // This function is triggered by the bubbleSlide() function below
-function bubbles(bi){
+function bubbles(bi)
+{
 	// Fade-out the content
 	_("bubblecontent").style.opacity = 0;
+
 	// Loop over the bubbles and change all of their background color
-	for(var i=0; i < ba.length; i++){
+	for(var i=0; i < ba.length; i++)
+	{
 		ba[i].style.background = "rgba(0,0,0,.1)";
 	}
 	// Change the target bubble background to be darker than the rest
 	ba[bi].style.background = "#999";
+
 	// Stall the bubble and content changing for just 300ms
-	setTimeout(function(){
+	setTimeout(function()
+	{
 		// Change the content
 		_("bubblecontent").innerHTML = bca[bi];
+	
 		// Fade-in the content
 		_("bubblecontent").style.opacity = 1;
 	}, 300);	
 }
+
 // This function is set to run every 5 seconds(5000ms)
-function bubbleSlide(){
+function bubbleSlide()
+{
 	bi++; // Increment the bubble index number
+	
 	// If bubble index number is equal to the amount of total bubbles
-	if(bi == ba.length){
+	if(bi == ba.length)
+	{
 		bi = 0; // Reset the bubble index to 0 so it loops back at the beginning
 	}
 	// Make the bubbles() function above run
 	bubbles(bi);
 }
+
 // Start the application up when document is ready
-window.addEventListener("load", function(){
+window.addEventListener("load", function()
+{
 	// Get the bubbles array
 	ba = _("bubbles").children;
+
 	// Set the interval timing for the slideshow speed
 	intrvl = setInterval(bubbleSlide, 5000);
 });
