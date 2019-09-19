13b8ab2d782bdbbafd678640a3a51552aaa1c866
Keo` <KeoNthite@gmail.com>
2019-09-19 13:26:36 +0200

    changed html file to PHP

---
+++ thank-you.php
@@ -0,0 +1,71 @@
+<?php
+	using('user_process.php');
+?>
+
+<html>
+ <head>
+	<title>Thank You</title>
+	
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.min.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+	 <!-- Bootstrap core JavaScript -->
+    <script src="js/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/docs.min.js"></script>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Bootstrap theme -->
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+     
+    <!-- Add icon library -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    		
+    <!-- Custom styles for this stylsheet -->
+	 <link href="css/stylesheet.css" rel="stylesheet" type="text/css">
+	 
+	 <!-- Form  validation code -->
+	<link rel="stylesheet" href="js/jquery-ui.css" />
+	<script src="js/jquery-1.8.3.js"></script>
+	<script src="js/jquery-ui.js"></script>
+	<script language="javascript" src="js/jquery.validation.js"></script>
+
+</head>
+
+<body>
+	<!-- nav bar -->
+	<div class="nav">
+	    <p id="nameHeader">Keoabetswe <strong>Nthite</strong></p>
+
+	    <label for="toggle">&#9776;</label>
+	    <input type="checkbox" id="toggle"/>
+	            
+	    <div class="menu">
+   			<a href="index.html">Home</a>
+	        <a href="about.html">About</a>
+   	        <a href="blog.php">Blog</a>
+	        <a href="portfolio.php">Portfolio</a>
+   	        <a href="contact.php">Contact</a>
+		</div>
+	</div>
+
+	<!-- break -->
+	<br/>
+	
+	<!-- maincontent row -->		         
+	<div id="maincontent" >		
+		<h2 id="contentHeader" style="color:gray;"></h2>
+	</div>		
+	
+</body>
+</html>
