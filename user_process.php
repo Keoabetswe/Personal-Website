6ffce6d041748a92c65c7fcd13c394701f89a549
Keoabetswe <KeoNthite@gmail.com>
2020-09-08 23:21:48 +0200

    update connection with heroku details

--- user_process.php
+++ user_process.php
@@ -1,10 +1,9 @@
 <?php
 // connects to the database
-$db_host="192.168.110.19";	
-$db_username="keontuzb_keo";	
-$db_password="Mm@mpeta1";	
-$db_name="keontuzb_website";
-
+$db_host="us-cdbr-east-02.cleardb.com";	
+$db_username="bd1f066e6f1964";	
+$db_password="108a612f";	
+$db_name="heroku_ad3b847ae807a1b";
 
 $db_connect = mysqli_connect($db_host, $db_username, $db_password, $db_name);	
 
