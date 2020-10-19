05cb190e755281e16c9c3f602e028886b8038d0a
Keoabetswe <KeoNthite@gmail.com>
2020-10-20 01:16:57 +0200

    rename about.html to about.php

---
+++ about.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>About</title>
+	<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
+	<link rel="icon" href="images/code.ico">		
+	
+	<!-- css -->
+	<link rel="stylesheet" href="css/stylesheet.css">
+	
+	<!-- Add icon library -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+	<div class="nav">
+	    <p id="website-name">Keoabetswe <strong>Nthite</strong></p>
+
+	    <label for="toggle">&#9776;</label>
+	    <input type="checkbox" id="toggle"/>
+	            
+	    <div class="menu">
+			<a href="index.php">Home</a>
+			<a href="about.html">About</a>
+			<a href="documents/K Nthite CV.pdf" target="_blank">CV</a>
+   	        <a href="blog.php">Blog</a>
+	        <a href="portfolio.php">Portfolio</a>
+   	        <a href="contact.php">Contact</a>
+	    </div>
+	</div>
+
+<!-- break -->		
+<br/>
+
+	<!-- main content-->
+	<div id="container">           
+		<div id="maincontent" class="col-md-8">
+			<h2 id="body-title">About</h2>
+				<p>I'm Keoabetswe Nthite a software developer and student at CPUT(Cape Peninsula University of Technology) in Cape Town, currently doing my BTech in I.T(Software Development).</p>
+					
+				<p>I currently hold a <a id="attachment-link" href="documents/K. NTHITE CV.pdf" target="_blank">Diploma in I.T (Software Development)</a> from Varsity College in Pretoria and I'm now pursuing a Degree. 
+					I also volunteer at <a id="coderdojo-link" href="https://coderdojo.com/" target="_blank">CoderDojo</a>, 
+					which is a programming club that teaches young people to code.</p>
+						
+				<p>My skills as a programmer allow me to work on several platforms such as web development, Android applications, and computer programs.</p>
+	
+				<strong><p>Tech Stack:</p></strong>
+						
+					<div id="tech-stack-list">
+						<ul>
+							<li>Java</li>
+							<li>HTML/CSS</li>
+							<li>Bootstrap</li>
+							<li>JavaScript</li>
+							<li>PHP</li>		
+							<li>MySQL</li>
+							<li>Git</li>
+						</ul>
+					</div>	
+						
+	<!-- closes content -->
+	</div>
+
+			<div id="sidebar" class="col-md-4">
+				<p>PDF Downloads</p>
+				<center><a id="attachment-link" href="documents/K. NTHITE CV.pdf" target="_blank">Curriculum Vitae</a></center>	
+				<center><a id="attachment-link" href="documents/Diploma in I.T.pdf" target="_blank">Qualification</a></center>	
+			</div>
+
+				
+			<!-- Sidebar content (large screen) -->
+			<div id="sidebar" class="col-md-4">
+				<div id="sidebar-image">
+					<div class="panel-body">
+						<img src="images/keo_grad.jpg" alt="Qualification" class="img-thumbnail aboutImg">
+					</div>
+				</div>		
+			</div>
+			
+		<!-- footer -->
+		<div id="footer" class="col-lg-12">
+			<center>
+				<a href="https://www.linkedin.com/in/keoabetswe-nthite-a4a6b3ab/" class="fa fa-linkedin" title="LinkedIn" target="_blank"></a> 
+				<a href="https://github.com/Keoabetswe" class="fa fa-github" title="GitHub" target="_blank"></a>
+				<a href="https://medium.com/@keonthite" class="fa fa-medium" title="Medium" target="_blank"></a>  				
+				<a href="contact.php" class="fa fa-envelope" title="Email"></a>
+			</center>
+			
+		</div>	
+	</div>
+
+</body>
+</html>
