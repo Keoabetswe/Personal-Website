01c131246a97fa55998c19ef2cc8195f091ffa33
Keoabetswe <KeoNthite@gmail.com>
2023-02-04 21:11:39 +0200

    remove db connection strings

--- createProject.php
+++ createProject.php
@@ -1,9 +1,10 @@
 ﻿<?php
 session_start();
-$db_host="us-cdbr-east-02.cleardb.com";	
-$db_username="bd1f066e6f1964";	
-$db_password="108a612f";	
-$db_name="heroku_ad3b847ae807a1b";
+$db_host = "";
+$db_username = "";
+$db_password = "";
+$db_name = "";
+$db_connect = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 
 
 if (isset($_GET['logout'])) 
